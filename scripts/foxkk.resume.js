0b56b8cb3cc6702bda8684e0c7bd1c69151a3e41
foxkk <31717088+foxkk@users.noreply.github.com>
2017-11-10 19:07:22 +0800

    resume

--- scripts/foxkk.resume.js
+++ scripts/foxkk.resume.js
@@ -16,8 +16,21 @@ $(document).ready(function(){
    console.log($('.resume').height());
 });
 window.foxkk = {
-    init : function(){
-        console.error('hucheng');
+    'device_width' : 0,
+    'device_height' : 0,
+    'init' : function(){
+        this.device_height = $(window).innerHeight();
+        this.device_width = $(window).innerWidth();
+        alert('height : '+ this.device_height + '  -- width : ' +this.device_width);
+        return this;
+    },
+    'initResume' : function(){
+        console.log('-------------------- 求职简历 ----------------------------------');
+        console.log('------------------ 姓名 : 胡呈 ---------------------------------');
+        console.log('------------------ 职位 : PHP高级工程师 -------------------------');
+        console.log('------------------ 电话 : 186 8020 9067 ------------------------');
+        console.log('------------------ 邮箱 : hucheng95@126.com --------------------');
+        return this;
     }
-}.init();
+}.initResume().init();
 console.timeEnd('总运行时间:');
