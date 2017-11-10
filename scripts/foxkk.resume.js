d0ee3fad69ce4de17e637189ff90548bc28ca581
foxkk <31717088+foxkk@users.noreply.github.com>
2017-11-10 22:59:57 +0800

    resume

--- scripts/foxkk.resume.js
+++ scripts/foxkk.resume.js
@@ -11,30 +11,58 @@
  * | @[copyright] : foxkk@copyright
  * +----------------------------------------------------------------------
  **/
+kk = jQuery.noConflict();
 console.time('总运行时间:');
-$(document).ready(function(){
-   console.log($('.resume').height());
+kk(document).ready(function(){
+    foxkk.init().initResume().initRem(window,document).initContainer('.resume');
 });
 window.foxkk = {
+    'max_width':960,
+    'max_height' : 0,
     'device_width' : 0,
     'device_height' : 0,
     'init' : function(){
-        this.device_height = $(window).innerHeight();
-        this.device_width = $(window).innerWidth();
-        alert('height : '+ this.device_height + '  -- width : ' +this.device_width);
+        this.device_height = kk(window).innerHeight();
+        this.device_width = kk(window).innerWidth();
         return this;
     },
     'initRem' : function(window,document){
-        var element = document.documentElement;
-        var dpr = window.devicePixelRatio || 1;
-        if (document.body) {
-            document.body.style.fontSize = (12 * dpr) + 'px'
-        } else {
-            document.addEventListener('DOMContentLoaded', setBodyFontSize)
+        var docEl = document.documentElement
+        var dpr = window.devicePixelRatio || 1
+        setBodyFontSize();
+        setRemUnit();
+        // reset rem unit on page resize
+        window.addEventListener('resize', setRemUnit);
+        window.addEventListener('pageshow', function (e) {
+            if (e.persisted) {
+                setRemUnit()
+            }
+        });
+        // detect 0.5px supports
+        if (dpr >= 2) {
+            var fakeBody = document.createElement('body');
+            var testElement = document.createElement('div');
+            testElement.style.border = '.5px solid transparent';
+            fakeBody.appendChild(testElement);
+            docEl.appendChild(fakeBody);
+            if (testElement.offsetHeight === 1) {
+                docEl.classList.add('hairlines');
+            }
+            docEl.removeChild(fakeBody);
         }
-        var rem = element.clientWidth / 10
-        element.style.fontSize = rem + 'px'
-
+        // adjust body font size
+        function setBodyFontSize () {
+            if (document.body) {
+                document.body.style.fontSize = (12 * dpr) + 'px'
+            } else {
+                document.addEventListener('DOMContentLoaded', setBodyFontSize)
+            }
+        }
+        // set 1rem = viewWidth / 10
+        function setRemUnit () {
+            var rem = docEl.clientWidth / 10
+            docEl.style.fontSize = rem + 'px'
+        }
         return this;
     },
     'initResume' : function(){
@@ -44,7 +72,15 @@ window.foxkk = {
         console.log('------------------ 电话 : 186 8020 9067 ------------------------');
         console.log('------------------ 邮箱 : hucheng95@126.com --------------------');
         return this;
+    },
+    'initContainer' : function(element){
+        var width = this.device_width >= this.max_width ? this.max_width : this.device_width;
+        var height = this.device_height;
+        kk(element).css({
+            'width':width+'px',
+            'height' : height+'px',
+        });
+        return this;
     }
 };
-foxkk.initResume().init();
 console.timeEnd('总运行时间:');
