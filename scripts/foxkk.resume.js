960ea9d445734a66c3ac9ee35c87cb062d46ff49
foxkk <31717088+foxkk@users.noreply.github.com>
2017-11-17 22:18:45 +0800

    resume

--- scripts/foxkk.resume.js
+++ scripts/foxkk.resume.js
@@ -13,6 +13,6 @@
  **/
 console.time('总运行时间:');
 k(document).ready(function(){
-    foxkk.init().initResume().initRem().initContainer('.resume').initResize('.resume');
+    foxkk.init().initResume().initRem().initContainer('.resume1').initResize('.resume1');
 });
 console.timeEnd('总运行时间:');
