b19fe7105a2203c1595e9e15a255601e81ec5801
foxkk <31717088+foxkk@users.noreply.github.com>
2017-11-19 14:29:45 +0800

    resume

--- scripts/foxkk.resume.js
+++ scripts/foxkk.resume.js
@@ -14,5 +14,13 @@
 console.time('总运行时间:');
 k(document).ready(function(){
     foxkk.init().initResume().initRem().initContainer('.resume1').initResize('.resume1');
+    var swiper = new Swiper('.swiper-container', {
+        autoHeight: true, //enable auto height
+        spaceBetween: 20,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        }
+    });
 });
 console.timeEnd('总运行时间:');
