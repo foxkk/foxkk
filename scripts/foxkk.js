de3b7279c53e27acddc70f780a3005b740eab033
foxkk <31717088+foxkk@users.noreply.github.com>
2017-11-30 14:15:21 +0800

    resume

--- scripts/foxkk.js
+++ scripts/foxkk.js
@@ -13,14 +13,17 @@
  **/
 k = jQuery.noConflict();
 k(document).ready(function(){
-    foxkk.init().initResume().initRem().fullScreen('.cover').initCover('.cover');
+    foxkk.init().initResume().fullScreen('.cover').initMask('.mask',10);
 });
 window.foxkk = {
+    'prefix':'items',
     'top' : 0,
     'left': 0,
     'rem' : 0,
     'elements' : [],
     'base_width' : 320,
+    'valid_width' : 0,
+    'doc_height':0,
     'base_font_size' : 12,
     'max_width':960,
     'max_height' :1024,
@@ -28,18 +31,29 @@ window.foxkk = {
     'min_height' : 480,
     'device_width' : 0,
     'device_height' : 0,
-    'position':null,
+    'words':[
+        '自信','乐观','细心','善良','诚信',
+        '谈定','创新','责任','风度','帅气',
+        '年轻','梦想','感恩','好学','理智',
+        '豁达','执着','宽容','激情','热情',
+        '爱心','直率','斗志','率真','倾听'
+    ],
+    'colors':[
+        'rgb(27,3,3)','rgb(211,73,50)','rgb(240,135,29)',
+        'rgb(72,192,234)','rgb(189,214,57)','rgb(5,0,0)',
+        'rgb(94,49,143)','rgb(69,43,31)','rgb(52,6,8)',
+        'rgb(57,39,38)','rgb(205,184,206)','rgb(205,39,206)'
+    ],
     'init' : function(){
+        var html = document.documentElement;
         this.device_height = k(window).innerHeight();
         this.device_width = k(window).innerWidth();
-        return this;
-    },
-    'initRem' : function(){
-        var html = document.documentElement;
-        var width = this.device_width >= this.max_width ? this.max_width : this.device_width;
-        this.rem = Math.ceil(width*this.base_font_size/this.base_width);
+        this.doc_height = k(document).innerHeight();
+        this.valid_width = this.device_width >= this.max_width ? this.max_width : this.device_width;
+        this.rem = Math.ceil( this.valid_width*this.base_font_size/this.base_width);
         this.rem = this.rem % 2 == 0 ? this.rem : this.rem + 1;
         html.style.fontSize = this.rem + 'px';
+
         return this;
     },
     'initResume' : function(){
@@ -48,6 +62,7 @@ window.foxkk = {
         console.log('------------------ 职位 : PHP高级工程师 -------------------------');
         console.log('------------------ 电话 : 186 8020 9067 ------------------------');
         console.log('------------------ 邮箱 : hucheng95@126.com --------------------');
+
         return this;
     },
     'initResize' : function(element){
@@ -55,7 +70,7 @@ window.foxkk = {
             window.foxkk.elements[element] = k(element);
         }
         k(window).resize(function(){
-            foxkk.initRem();
+            foxkk.init();
         });
     },
     'fullScreen' : function(element) {
@@ -70,22 +85,14 @@ window.foxkk = {
     'citem':null,
     'initCover':function(element){
         /*15--8*/
-        var rows = 15;
         var columns = 8;
-        var colors = [];
-            colors[5] = 'rgb(27,3,3)';
-            colors[4] = 'rgb(211,73,50)';
-            colors[3] = 'rgb(240,135,29)';
-            colors[2] = 'rgb(72,192,234)';
-            colors[1] = 'rgb(189,214,57)';
-            colors[0] = 'rgb(5,0,0)';
-
         var animations = ['animation01','animation02'];
+
         var datas = new Array();
             for(var i = 0;i<rows;i++){
                 datas[i] = new Array();
             }
-            datas[3][5] = {'text':'求','class':'animation opciment'};
+            datas[3][5] = {'text':'求'};
             datas[4][5] = {'text':'职'};
             datas[5][5] = {'text':'简'};
             datas[6][5] = {'text':'历'};
@@ -155,6 +162,38 @@ window.foxkk = {
                 }
             }
     },
+    'initMask' : function(parent,columns){
+        columns = columns || 8;
+        var p_item = k(parent);
+        var item_width = (this.valid_width/columns).toFixed(6);
+        var rows = Math.ceil(this.doc_height/item_width);
+
+        var items = new Array();
+        for(var i = 0;i<rows;i++){
+            items[i] = new Array();
+        }
+        for(var r = 0; r < rows; r++){
+            for(var c = 0; c < columns; c++){
+                var word_index = Math.floor(Math.random()*this.words.length);
+                var color_index = Math.floor(Math.random()*this.colors.length);
+                var item = "<span class='item'>"+this.words[word_index]+"</span>"
+                var temp = k(item);
+                var top = r*item_width;
+                var left = c*item_width;
+                temp.css({
+                    'width':item_width+'px',
+                    'height':item_width+'px',
+                    'line-height':item_width+'px',
+                    'left':left+'px',
+                    'top':top+'px',
+                    'background':this.colors[color_index]
+                });
+                p_item.append(temp);
+                items[r][c] = {'items':temp,'visible':1,'position':{'top':top,'left':left}};
+            }
+        }
+        this.elements[this.prefix+parent] = items;
+    },
     'keys' : function(object){
 
     },
@@ -164,8 +203,4 @@ window.foxkk = {
            this.left =  k(document).scrollLeft();
        });
     }
-
 };
-function setItemClass(item){
-    item.addClass('anim_fade_out');
-}
